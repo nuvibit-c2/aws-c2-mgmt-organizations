af412b3479dcc69d85f75d574dd8c7abc41068af
stefano-franco <stefano.franco@nuvibit.com>
2023-03-31 22:58:19 +0200

    chore: migrating to external module

--- poc_core_parameters.tf
+++ poc_core_parameters.tf
@@ -15,7 +15,7 @@ locals {
 # ¦ CORE PARAMETERS S3
 # ---------------------------------------------------------------------------------------------------------------------
 module "core_parameters_s3" {
-  source = "./modules/core-parameters-s3"
+  source = "github.com/nuvibit/terraform-aws-core-parameters-s3?ref=feat-init"
 
   bucket_name     = "poc-core-parameters-s3"
   org_id          = data.aws_organizations_organization.current.id
@@ -25,11 +25,11 @@ module "core_parameters_s3" {
 # ---------------------------------------------------------------------------------------------------------------------
 # ¦ CORE PARAMETERS S3 - READER
 # ---------------------------------------------------------------------------------------------------------------------
-# module "core_parameters_s3_reader" {
-#   source = "./modules/core-parameters-s3"
+module "core_parameters_s3_reader" {
+  source = "github.com/nuvibit/terraform-aws-core-parameters-s3//modules/reader?ref=feat-init"
 
-#   bucket_name = "poc-core-parameters-s3"
-# }
+  bucket_name = "poc-core-parameters-s3"
+}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # ¦ CORE PARAMETERS S3 - WRITER

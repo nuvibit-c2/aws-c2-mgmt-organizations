ae46d2d2eaf5d10d79d6a2049f003ded79bdff93
stefano-franco <stefano.franco@nuvibit.com>
2023-04-23 23:15:54 +0200

    update account factory baseline

--- ntc_account_factory.tf
+++ ntc_account_factory.tf
@@ -4,16 +4,33 @@
 module "account_factory" {
   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-account-factory?ref=beta"
 
-  # baseline_scopes = [
-  #   {
-  #     scope_name = "workload_prod_euc1_use1"
-  #     terraform_version = "1.3.9"
-  #     aws_provider_version = "4.59.0"
-  #     baseline_regions = ["us-east-1", "eu-central-1"]
-  #     baseline_target_ou_path = "/root/workloads/prod"
-  #     baseline_source_file_path = ""
-  #   }
-  # ]
+  baseline_bucket_name = "aws-c2-ntc-account-factory-baseline"
+  baseline_scopes = [
+    {
+      scope_name           = "workloads_prod"
+      terraform_version    = "1.3.9"
+      aws_provider_version = "4.59.0"
+      decommission         = false
+      terraform_baseline_contents = [
+        file("${path.module}/baseline-examples/baseline_data.tf")
+      ]
+      regions = ["us-east-1", "eu-central-1"]
+      # decide which AWS accounts should be added to this baseline scope
+      target_ou_paths = [
+        "/root/workloads/prod"
+      ]
+      target_account_names = [
+        # "aws-c2-0001",
+        # "aws-c2-0002"
+      ]
+      target_account_tags = [
+        # {
+        #   key = "AccountType"
+        #   value = "core"
+        # }
+      ]
+    }
+  ]
 
   providers = {
     aws = aws.euc1

ef5eded6236a845a3c1cf60df0f1d361cd7a7c22
stefano-franco <stefano.franco@nuvibit.com>
2024-02-08 11:26:57 +0100

    import

--- import.tf
+++ import.tf
@@ -1,14 +1,32 @@
 import {
-    to = module.organizations.aws_organizations_organization.ntc_org[0]
-    id = "o-m29e8d9awz"
+    to = module.organizations.aws_organizations_organizational_unit.ntc_nested_ou_level1["/root/workloads"]
+    id = "ou-6gf5-6ltp3mjf"
 }
-
 import {
-    to = module.organizations.module.org_cloudtrail[0].aws_cloudtrail.ntc_cloudtrail
-    id = "arn:aws:cloudtrail:eu-central-1:228120440352:trail/organization-trail"
+    to = module.organizations.aws_organizations_organizational_unit.ntc_nested_ou_level1["/root/infrastructure"]
+    id = "ou-6gf5-uktkya48"
 }
-
 import {
-    to = aws_iam_role.ntc_org_account_reader
-    id = "ntc-org-account-reader"
+    to = module.organizations.aws_organizations_organizational_unit.ntc_nested_ou_level1["/root/security"]
+    id = "ou-6gf5-ebbpq9yb"
+}
+import {
+    to = module.organizations.aws_organizations_organizational_unit.ntc_nested_ou_level1["/root/sandbox"]
+    id = "ou-6gf5-yvm8rkvb"
+}
+import {
+    to = module.organizations.aws_organizations_organizational_unit.ntc_nested_ou_level1["/root/suspended"]
+    id = "ou-6gf5-hyl8xkvz"
+}
+import {
+    to = module.organizations.aws_organizations_policy.ntc_scp["scp_workloads_ou"]
+    id = "p-u4s7wj22"
+}
+import {
+    to = module.organizations.aws_organizations_policy.ntc_scp["scp_suspended_ou"]
+    id = "p-dgcq8mhw"
+}
+import {
+    to = module.organizations.aws_organizations_policy.ntc_scp["scp_root_ou"]
+    id = "p-ew4wrqqn"
 }

e9c34cf2d5d48b625ac6948af8e109c1ce5f1069
stefano-franco <stefano.franco@nuvibit.com>
2024-02-08 11:30:04 +0100

    import

--- import.tf
+++ import.tf
@@ -1,12 +1,12 @@
 import {
     to = module.organizations.aws_organizations_organizational_unit.ntc_nested_ou_level2["/root/workloads/dev"]
-    id = ""
+    id = "ou-6gf5-1mp869fj"
 }
 import {
     to = module.organizations.aws_organizations_organizational_unit.ntc_nested_ou_level2["/root/workloads/prod"]
-    id = ""
+    id = "ou-6gf5-xrerrg1c"
 }
 import {
     to = module.organizations.aws_organizations_organizational_unit.ntc_nested_ou_level2["/root/workloads/test"]
-    id = ""
+    id = "ou-6gf5-5ejrrc90"
 }

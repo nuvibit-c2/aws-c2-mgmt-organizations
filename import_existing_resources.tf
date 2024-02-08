a1bc72addd1edbac0974f3d88e0f9afb695fd8bb
stefano-franco <stefano.franco@nuvibit.com>
2024-02-08 16:13:53 +0100

    adding import placeholder

---
+++ import_existing_resources.tf
@@ -0,0 +1,10 @@
+# existing resources can be imported to be managed by NTC - this can be usefull to avoid downtime or recreating critical resources
+# import blocks are only available in Terraform v1.5.0 and later
+
+/*
+import {
+  # AWS Organization cannot simply be deleted if accounts exist and is therefore easier to import
+  to = module.organizations.aws_organizations_organization.ntc_org[0]
+  id = "o-m29e8d9awz"
+}
+*/

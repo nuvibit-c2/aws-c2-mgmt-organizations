8cde666c00137c2029ce747a1c4ebaf27bcbeb12
stefano-franco <stefano.franco@nuvibit.com>
2024-10-10 23:40:02 +0200

    reprovision admin delegations

--- ntc_organizations_admin_delegations.tf
+++ ntc_organizations_admin_delegations.tf
@@ -20,32 +20,32 @@ locals {
 # ---------------------------------------------------------------------------------------------------------------------
 # ¦ NTC ORGANIZATIONS - ADMINI DELEGATIONS
 # ---------------------------------------------------------------------------------------------------------------------
-# module "ntc_delegated_admins_euc1" {
-#   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-organizations//modules/regional-admin-delegations?ref=1.3.0"
+module "ntc_delegated_admins_euc1" {
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-organizations//modules/regional-admin-delegations?ref=1.3.0"
 
-#   delegated_administrators = local.delegated_security_services
+  delegated_administrators = local.delegated_security_services
 
-#   providers = {
-#     aws = aws.euc1
-#   }
-# }
+  providers = {
+    aws = aws.euc1
+  }
+}
 
-# module "ntc_delegated_admins_euc2" {
-#   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-organizations//modules/regional-admin-delegations?ref=1.3.0"
+module "ntc_delegated_admins_euc2" {
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-organizations//modules/regional-admin-delegations?ref=1.3.0"
 
-#   delegated_administrators = local.delegated_security_services
+  delegated_administrators = local.delegated_security_services
 
-#   providers = {
-#     aws = aws.euc2
-#   }
-# }
+  providers = {
+    aws = aws.euc2
+  }
+}
 
-# module "ntc_delegated_admins_use1" {
-#   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-organizations//modules/regional-admin-delegations?ref=1.3.0"
+module "ntc_delegated_admins_use1" {
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-organizations//modules/regional-admin-delegations?ref=1.3.0"
 
-#   delegated_administrators = local.delegated_security_services
+  delegated_administrators = local.delegated_security_services
 
-#   providers = {
-#     aws = aws.use1
-#   }
-# }
+  providers = {
+    aws = aws.use1
+  }
+}

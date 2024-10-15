f4f8bafb5b1f0b1da607aa9604c62f7e862b1a6d
stefano-franco <stefano.franco@nuvibit.com>
2024-10-15 17:06:39 +0200

    update delegation

--- ntc_organizations_admin_delegations.tf
+++ ntc_organizations_admin_delegations.tf
@@ -1,22 +1,25 @@
 locals {
-  # security services that should be delegated to central security account
-  # WARNING: firewall manager can only be delegated in us-east-1
-  delegated_administrators = [
+  # some services like aws config are delegated once and not for each region
+  global_delegated_administrators = [
     {
       service_principal = "config.amazonaws.com"
-      admin_account_id  = local.ntc_parameters["mgmt-account-factory"]["core_accounts"]["aws-c2-security"]
+      admin_account_id  = local.ntc_parameters["mgmt-account-factory"]["core_accounts"]["nivel-int-security-tooling"]
     },
+  ]
+  
+  # some services like amazon guardduty need to be delegated for each region
+  regional_delegated_administrators = [
     {
       service_principal = "securityhub.amazonaws.com"
-      admin_account_id  = local.ntc_parameters["mgmt-account-factory"]["core_accounts"]["aws-c2-security"]
+      admin_account_id  = local.ntc_parameters["mgmt-account-factory"]["core_accounts"]["nivel-int-security-tooling"]
     },
     {
       service_principal = "guardduty.amazonaws.com"
-      admin_account_id  = local.ntc_parameters["mgmt-account-factory"]["core_accounts"]["aws-c2-security"]
+      admin_account_id  = local.ntc_parameters["mgmt-account-factory"]["core_accounts"]["nivel-int-security-tooling"]
     },
     {
       service_principal = "inspector2.amazonaws.com"
-      admin_account_id  = local.ntc_parameters["mgmt-account-factory"]["core_accounts"]["aws-c2-security"]
+      admin_account_id  = local.ntc_parameters["mgmt-account-factory"]["core_accounts"]["nivel-int-security-tooling"]
     }
   ]
 }
@@ -27,7 +30,7 @@ locals {
 module "ntc_delegated_admins_euc1" {
   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-organizations//modules/regional-admin-delegations?ref=1.3.0"
 
-  delegated_administrators = local.delegated_administrators
+  delegated_administrators = concat(local.global_delegated_administrators, local.regional_delegated_administrators)
 
   providers = {
     aws = aws.euc1
@@ -37,7 +40,7 @@ module "ntc_delegated_admins_euc1" {
 module "ntc_delegated_admins_euc2" {
   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-organizations//modules/regional-admin-delegations?ref=1.3.0"
 
-  delegated_administrators = local.delegated_administrators
+  delegated_administrators = local.regional_delegated_administrators
 
   providers = {
     aws = aws.euc2
@@ -47,7 +50,7 @@ module "ntc_delegated_admins_euc2" {
 module "ntc_delegated_admins_use1" {
   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-organizations//modules/regional-admin-delegations?ref=1.3.0"
 
-  delegated_administrators = local.delegated_administrators
+  delegated_administrators = local.regional_delegated_administrators
 
   providers = {
     aws = aws.use1

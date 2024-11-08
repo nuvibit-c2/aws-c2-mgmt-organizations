1319c7a4d268cc8b654c0c36fcc3bf57c6e9f4e2
stefano-franco <stefano.franco@nuvibit.com>
2024-11-08 22:58:45 +0100

    init access analyzer

--- ntc_organizations_admin_delegations.tf
+++ ntc_organizations_admin_delegations.tf
@@ -28,8 +28,15 @@ locals {
   ]
 }
 
+# organizations integration of iam access analyzer requires a service linked role in org management account
+# this role gets created when creating up an access analyzer for the first time (analyzer can be deleted afterwards)
+# https://docs.aws.amazon.com/IAM/latest/UserGuide/access-analyzer-using-service-linked-roles.html#create-slr
+resource "aws_accessanalyzer_analyzer" "init" {
+  analyzer_name = "init-service-linked-role"
+}
+
 # ---------------------------------------------------------------------------------------------------------------------
-# ¦ NTC ORGANIZATIONS - ADMINI DELEGATIONS
+# ¦ NTC ORGANIZATIONS - ADMIN DELEGATIONS
 # ---------------------------------------------------------------------------------------------------------------------
 module "ntc_delegated_admins_euc1" {
   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-organizations//modules/regional-admin-delegations?ref=1.3.0"

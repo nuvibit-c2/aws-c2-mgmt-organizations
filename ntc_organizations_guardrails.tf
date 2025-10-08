d932232b105b7ce5cbeba9112ba701efef3d04fc
burib <burib@users.noreply.github.com>
2025-10-08 18:32:42 +0200

    feat: deny iam user and key creation with SCP
    deny iam user and key creation with SCP through guardrail templates 1.3.0

--- ntc_organizations_guardrails.tf
+++ ntc_organizations_guardrails.tf
@@ -2,19 +2,21 @@
 # ¦ NTC GUARDRAIL TEMPLATES
 # ---------------------------------------------------------------------------------------------------------------------
 module "ntc_guardrail_templates" {
-  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-guardrail-templates?ref=1.2.1"
+   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-guardrail-templates?ref=1.3.0"
 
   # service control policies (SCPs) can apply permission guardrails at the organization, organizational unit, or account level
   # https://docs.aws.amazon.com/organizations/latest/userguide/orgs_manage_policies_scps.html
   service_control_policy_templates = [
     {
       policy_name        = "scp_root_ou"
-      policy_description = "Deny leaving the organization and root user actions except for centralized root privilege tasks"
+      policy_description = "Deny member accounts from leaving the organization and any root user actions except for centralized root privilege tasks"
       target_ou_paths    = ["/root"]
       template_names = [
         "deny_leaving_organizations",
-        "deny_actions_as_root_except_centralized_root"
+        "deny_actions_as_root_except_centralized_root",
+        "deny_iam_user_and_access_key_creation"
       ]
+      exclude_principal_arns = ["arn:aws:iam::*:role/OrganizationAccountAccessRole"]
     },
     {
       policy_name        = "scp_suspended_ou"
